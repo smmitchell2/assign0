b6184bca0fb9334e52a2a3f30e03b3ddae9dd7d9
smmitchell2 <smmitchell3@gmail.com>
2017-05-18 14:05:47 -0500

    da works

--- da.h
+++ da.h
@@ -6,10 +6,10 @@
 
 typedef struct DA DA;
 
-extern DA *newDArray(void (*display)(FILE *,void *));
+extern DA *newDA(void (*display)(FILE *,void *));
 extern void insertDA(DA *a,void *v);
 extern void *removeDA(DA *a);
-extern void unionDa(DA *recipient, DA *donor);
+extern void unionDA(DA *recipient, DA *donor);
 extern void *getDA(DA *a,int index);
 extern void setDA(DA *a,int index,void *value);
 extern int sizeDA(DA *a);
